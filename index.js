3486dfb56656090499b0dcd4eb2fe92b079d1a60
Alex-A-B <alexjamesbelton@gmail.com>
2021-05-25 15:18:12 +0100

    working through lab, challenges being undertaken.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+const createEmployeeRecord = (newEmployee) => {
+    return {
+        firstName: newEmployee[0],
+        familyName: newEmployee[1],
+        title: newEmployee[2],
+        payPerHour: newEmployee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+};
+
+const createEmployeeRecords = (employees) => {
+    return employees.map(person => createEmployeeRecord(person))
+}
+
+const createTimeInEvent = (employee, timeStamp) => {
+    let dateTime = timeStamp.split(" ");
+    let inHour = parseInt(dateTime[1], 10);
+    let inDate = dateTime[0];
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: inHour,
+        date: inDate
+    });
+    return employee
+}
